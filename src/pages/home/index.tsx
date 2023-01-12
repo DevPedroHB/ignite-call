84cc551735ba09754ee29479da799f11254d0b6b
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-01-12 17:40:52 -0300

    Home form visual structure and validation

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,31 @@
+import { Heading, Text } from "@ignite-ui/react";
+import Image from "next/image";
+import previewImage from "../../assets/app-preview.png";
+import { ClaimUsernameForm } from "./components/ClaimUsernameForm";
+import { Container, Hero, Preview } from "./styles";
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+        <ClaimUsernameForm />
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  );
+}
