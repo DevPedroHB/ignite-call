cd651c234fb97b04fcdf187f294dafb62a96a50b
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-01-13 19:10:34 -0300

    calendar and availability

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,5 +1,8 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import "../lib/dayjs";
+import { queryClient } from "../lib/react-query";
 import { globalStyles } from "../styles/global";
 
 globalStyles();
@@ -9,8 +12,10 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   );
 }
---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,74 @@
+import { useQuery } from "@tanstack/react-query";
+import dayjs from "dayjs";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { Calendar } from "../../../../../components/Calendar";
+import { api } from "../../../../../lib/axios";
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from "./styles";
+
+interface Availability {
+  possibleTimes: number[];
+  availableTimes: number[];
+}
+
+export function CalendarStep() {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const router = useRouter();
+  const isDateSelected = !!selectedDate;
+  const username = String(router.query.username);
+  const weekDay = selectedDate ? dayjs(selectedDate).format("dddd") : null;
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format("DD[ de ]MMMM")
+    : null;
+
+  const selectedDateWithoutTime = selectedDate
+    ? dayjs(selectedDate).format("YYYY-MM-DD")
+    : null;
+
+  const { data: availability } = useQuery<Availability>(
+    ["availability", selectedDateWithoutTime],
+    async () => {
+      const response = await api.get(`/users/${username}/availability`, {
+        params: {
+          date: selectedDateWithoutTime,
+        },
+      });
+
+      return response.data;
+    },
+    {
+      enabled: !!selectedDate,
+    }
+  );
+
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            {weekDay} <span>{describedDate}</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            {availability?.possibleTimes.map((hour) => {
+              return (
+                <TimePickerItem
+                  key={hour}
+                  disabled={!availability.availableTimes.includes(hour)}
+                >
+                  {String(hour).padStart(2, "0")}:00h
+                </TimePickerItem>
+              );
+            })}
+          </TimePickerList>
+        </TimePicker>
+      )}
+    </Container>
+  );
+}
